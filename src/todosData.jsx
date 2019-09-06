c8c57053f12ea4e052b8b9b677893bbd59ac532c
Reddie <r.villariza@hotmail.com>
2019-09-06 17:12:37 -0400

    first commit

---
+++ src/todosData.jsx
@@ -0,0 +1,29 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Take out the trash",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "Grocery shopping",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Go to the bank",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "Clean up storage",
+    completed: false
+  },
+  {
+    id: 5,
+    text: "Do laundry",
+    completed: false
+  }
+];
+
+export default todosData;
